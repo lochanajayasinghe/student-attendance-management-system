e4f3edf608f320efb2ad2ac57d4ed883f689c4db
lochanajayasinghe <159622233+lochanajayasinghe@users.noreply.github.com>
2025-07-08 23:41:47 +0530

    Update UserRepository.java

--- src/main/java/com/example/AMS/repository/UserRepository.java
+++ src/main/java/com/example/AMS/repository/UserRepository.java
@@ -1,6 +1,6 @@
-package com.example.Login.repository;
+package com.example.AMS.repository;
 
-import com.example.Login.model.User;
+import com.example.AMS.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
