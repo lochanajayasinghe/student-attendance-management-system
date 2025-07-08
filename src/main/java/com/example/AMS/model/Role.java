dc02d8d47768692a4aa6df283e7719f2cb146ed8
lochanajayasinghe <159622233+lochanajayasinghe@users.noreply.github.com>
2025-07-08 23:33:46 +0530

    Create Role.java

---
+++ src/main/java/com/example/AMS/model/Role.java
@@ -0,0 +1,16 @@
+package com.example.AMS.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
