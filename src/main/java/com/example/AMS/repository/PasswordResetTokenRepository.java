dec8b15917223829e701bf248d346507353053c6
lochanajayasinghe <159622233+lochanajayasinghe@users.noreply.github.com>
2025-07-08 23:39:17 +0530

    Update PasswordResetTokenRepository.java

--- src/main/java/com/example/AMS/repository/PasswordResetTokenRepository.java
+++ src/main/java/com/example/AMS/repository/PasswordResetTokenRepository.java
@@ -1,7 +1,7 @@
-package com.example.Login.repository;
+package com.example.AMS.repository;
 
-import com.example.Login.model.PasswordResetToken;
-import com.example.Login.model.User;
+import com.example.AMS.model.PasswordResetToken;
+import com.example.AMS.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
