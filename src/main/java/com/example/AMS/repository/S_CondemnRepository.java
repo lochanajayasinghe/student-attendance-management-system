d7d54c5e772809c18fe422279fa9cdc168c85fdb
Lochana Jayasinghe <lochanajayasinghe2002@gmail.com>
2025-07-27 23:31:37 +0530

    Create S_CondemnRepository.java

---
+++ src/main/java/com/example/AMS/repository/S_CondemnRepository.java
@@ -0,0 +1,7 @@
+package com.example.Login.repository;
+
+import com.example.Login.model.Condemn;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface S_CondemnRepository extends JpaRepository<Condemn, String> {
+}
